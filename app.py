cebf90663965f1b74d1ec5b92e2da8922fb26e74
Adrián Pabón Mendoza <apabon@optimizeit.co>
2024-07-11 14:50:18 -0500

    host

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def consulta_ticket_api():
 
 # si el script se ejecuta correctamente se ejecuta el servidor Flask
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0',port=5001, debug=True)
