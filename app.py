cea70cfa07858e5c121d5d85beae4852146c198d
Adrián Pabón Mendoza <apabon@optimizeit.co>
2024-07-17 17:51:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -97,12 +97,12 @@ def consulta_ticket_api(ticket_id):
             estado = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['STATUS']['#text']
             ticketID = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['TICKETID']
             description = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['DESCRIPTION']
-            # long_description = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['DESCRIPTION_LONGDESCRIPTION']
-            worklog = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']
+            long_description = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['DESCRIPTION_LONGDESCRIPTION']
+            # worklog = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']
             ownergroup = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['OWNERGROUP']
-            worklog_description = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['DESCRIPTION']
-            worklog_detalle = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['DESCRIPTION_LONGDESCRIPTION']
-            worklog_createdate = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['CREATEDATE']
+            #worklog_description = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['DESCRIPTION']
+            #worklog_detalle = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['DESCRIPTION_LONGDESCRIPTION']
+            #worklog_createdate = response_dict['soapenv:Envelope']['soapenv:Body']['QuerySRPROResponse']['SRPROSet']['SR']['WORKLOG']['CREATEDATE']
 
 
             
@@ -110,12 +110,12 @@ def consulta_ticket_api(ticket_id):
                 "TICKET": ticketID,
                 "ESTADO": estado,
                 "RESUMEN": description,
-               # "DETALLE": long_description,
-                "WORKLOG": worklog,
+               "DETALLE": long_description,
+                #"WORKLOG": worklog,
                 "GRUPO_RESOLUTOR": ownergroup,
-                "RESUMEN_WORKLOG": worklog_description,
-                "DETALLE_WORKLOG": worklog_detalle,
-                "FECHA_CREACION_WORKLOG": worklog_createdate
+                #"RESUMEN_WORKLOG": worklog_description,
+               #"DETALLE_WORKLOG": worklog_detalle,
+               # "FECHA_CREACION_WORKLOG": worklog_createdate
         }
             
 
